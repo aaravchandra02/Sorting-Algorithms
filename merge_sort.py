37e7f637d31ae4016329b8c8dd73f2f5b2711e0b
Aarav Chandra <aaravchandra02@gmail.com>
2020-09-24 16:17:54 -0700

    editted merge_sort to complete it

--- merge_sort.py
+++ merge_sort.py
@@ -25,7 +25,10 @@ def merge_sort(items):
     middle_index = len(items)//2
     left_split = items[:middle_index]
     right_split = items[middle_index:]
-    return middle_index, left_split, right_split
+    left_sorted = merge_sort(left_split)
+    right_sorted = merge_sort(right_split)
+
+    return merge(left_sorted, right_sorted)
 
 
 def merge(left, right):
