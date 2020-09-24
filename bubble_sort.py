b7abda1345e52a94fcaa41589faaf838f35221f2
Aarav Chandra <aaravchandra02@gmail.com>
2020-09-24 07:38:12 -0700

    basic code completed

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,5 @@
 nums = [5, 2, 9, 1, 5, 6]
 
-# Define swap() below:
-
 
 def swap(arr, index_1, index_2):
     temp = arr[index_1]
@@ -9,5 +7,16 @@ def swap(arr, index_1, index_2):
     arr[index_2] = temp
 
 
+def bubble_sort(arr):
+    for el in arr:
+        for index in range(len(arr)-1):
+            if arr[index] > arr[index+1]:
+                swap(arr, index, index+1)
+
+
 swap(nums, 3, 5)
 print(nums)
+
+print(f"Pre-Sort: {nums}")
+bubble_sort(nums)
+print("Post-Sort: {0}".format(nums))
