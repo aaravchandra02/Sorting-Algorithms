05e5a4b399e0c08bf0037a997eff88e17c8b6189
Aarav Chandra <aaravchandra02@gmail.com>
2020-09-24 16:21:03 -0700

    added test cases

--- merge_sort.py
+++ merge_sort.py
@@ -44,3 +44,26 @@ def merge(left, right):
         result += left
     if(right):
         result += right
+
+
+"""
+The whole sort takes up two functions:
+
+merge_sort() which is called recursively breaks down an input list to smaller, more manageable pieces. merge() which is a helper function built to help combine those broken-down lists into ordered combination lists.
+
+merge_sort() continues to break down an input list until it only has one element and then it joins that with other single element lists to create sorted 2-element lists. Then it combines 2-element sorted lists into 4-element sorted lists. It continues that way until all the items of the lists are sorted!
+
+Only one thing left to do, test it out!
+"""
+unordered_list1 = [356, 746, 264, 569, 949, 895, 125, 455]
+unordered_list2 = [787, 677, 391, 318, 543, 717, 180, 113, 795, 19,
+                   202, 534, 201, 370, 276, 975, 403, 624, 770, 595, 571, 268, 373]
+unordered_list3 = [860, 380, 151, 585, 743, 542, 147, 820, 439, 865, 924, 387]
+
+ordered_list1 = merge_sort(unordered_list1)
+ordered_list2 = merge_sort(unordered_list2)
+ordered_list3 = merge_sort(unordered_list3)
+
+print(ordered_list1)
+print(ordered_list2)
+print(ordered_list3)
