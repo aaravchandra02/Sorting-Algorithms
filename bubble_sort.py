5d6dfd2cdf1a2055fed8ed0e670eaceb84263b40
Aarav Chandra <aaravchandra02@gmail.com>
2020-09-24 07:48:00 -0700

    added optimized bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,4 +1,4 @@
-nums = [5, 2, 9, 1, 5, 6]
+nums = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 
 def swap(arr, index_1, index_2):
@@ -7,13 +7,35 @@ def swap(arr, index_1, index_2):
     arr[index_2] = temp
 
 
-def bubble_sort(arr):
+def bubble_sort_unoptimized(arr):
+    iteration_count = 0
     for el in arr:
         for index in range(len(arr)-1):
+            iteration_count += 1
             if arr[index] > arr[index+1]:
                 swap(arr, index, index+1)
+    print("PRE-OPTIMIZED ITERATION COUNT: {0}".format(iteration_count))
 
 
+print("PRE SORT: {0}".format(nums))
+
+
+def bubble_sort(arr):
+    iteration_count = 0
+    for i in range(len(arr)):
+        # iterate through unplaced elements
+        for idx in range(len(arr) - i - 1):
+            iteration_count += 1
+            if arr[idx] > arr[idx + 1]:
+                # replacement for swap function
+                arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+
+    print("POST-OPTIMIZED ITERATION COUNT: {0}".format(iteration_count))
+
+
+bubble_sort_unoptimized(nums.copy())
+bubble_sort(nums)
+print("POST SORT: {0}".format(nums))
 swap(nums, 3, 5)
 print(nums)
 
